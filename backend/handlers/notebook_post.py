941a0b1a2c6db391065e739d997f6bde03402f24
Jamique Campbell <43054485+jamique242@users.noreply.github.com>
2026-04-30 16:50:56 -0400

    Fix formatting in notepad_post function

--- backend/handlers/notebook_post.py
+++ backend/handlers/notebook_post.py
@@ -4,8 +4,6 @@ def notepad_post(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Day 1 API working"
-            "received": event
-          
+            "message": "Day 1 API working"          
         })
     }
