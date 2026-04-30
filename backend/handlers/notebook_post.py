00974d4497c75fad7e86797b98571e82d1f3b83b
Jamique Campbell <43054485+jamique242@users.noreply.github.com>
2026-04-30 16:29:35 -0400

    Add lambda_handler function to handle API requests

--- backend/handlers/notebook_post.py
+++ backend/handlers/notebook_post.py
@@ -1,1 +1,11 @@
+import json
 
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Day 1 API working"
+            "received": event
+          
+        })
+    }
