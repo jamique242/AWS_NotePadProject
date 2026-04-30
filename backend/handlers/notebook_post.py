f7c5271b16593b2f4ad70c743166455ecab391d3
Jamique Campbell <43054485+jamique242@users.noreply.github.com>
2026-04-30 16:50:35 -0400

    Remove 'received' field from API response

--- backend/handlers/notebook_post.py
+++ backend/handlers/notebook_post.py
@@ -5,7 +5,5 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "message": "Day 1 API working"
-            "received": event
-          
         })
     }
